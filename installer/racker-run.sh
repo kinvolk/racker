b8a180d7ad694aa5da61dc60f102be007ce1f5c8
Kai Lüke <kai@kinvolk.io>
2021-01-26 13:55:06 +0100

    installer: make extraction robust when run with | sh
    The "#!/bin/sh -x" line is ignored when execution happens on the fly
    with "| sh". Also, there is no terminal attached which makes docker -it
    fail.
    Move the error handling settings into a separate line and add make the
    docker invocation more robust.

--- installer/racker-run.sh
+++ installer/racker-run.sh
@@ -1,5 +1,6 @@
-#/bin/sh -e
+#/bin/sh
 
+set -eu
 # Setup the installer
 INSTALLER_DIR="$HOME/racker/"
 mkdir -p $INSTALLER_DIR
@@ -8,7 +9,7 @@ rm -rf $INSTALLER_DIR/*
 
 MOUNT_POINT=/racker-ext
 
-docker run -it -v $INSTALLER_DIR:$MOUNT_POINT:Z racker:latest tar -C "$MOUNT_POINT" -xzf /racker/racker.tar.gz
+docker run --rm -v $INSTALLER_DIR:$MOUNT_POINT:Z racker:latest tar -C "$MOUNT_POINT" -xzf /racker/racker.tar.gz
 
 # Run the installer
 pushd $INSTALLER_DIR > /dev/null
